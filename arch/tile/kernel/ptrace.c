2b38f7753f2a1653c655bd874f6d2f18f99764fe
Al Viro <viro@zeniv.linux.org.uk>
2012-12-07 18:58:16 -0500

    tile: don't open-code ptrace_report_syscall()
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/tile/kernel/ptrace.c
+++ arch/tile/kernel/ptrace.c
@@ -15,7 +15,7 @@
  */
 
 #include <linux/kernel.h>
-#include <linux/ptrace.h>
+#include <linux/tracehook.h>
 #include <linux/kprobes.h>
 #include <linux/compat.h>
 #include <linux/uaccess.h>
@@ -251,24 +251,7 @@ void do_syscall_trace(void)
 	if (!test_thread_flag(TIF_SYSCALL_TRACE))
 		return;
 
-	if (!(current->ptrace & PT_PTRACED))
-		return;
-
-	/*
-	 * The 0x80 provides a way for the tracing parent to distinguish
-	 * between a syscall stop and SIGTRAP delivery
-	 */
-	ptrace_notify(SIGTRAP|((current->ptrace & PT_TRACESYSGOOD) ? 0x80 : 0));
-
-	/*
-	 * this isn't the same as continuing with a signal, but it will do
-	 * for normal use.  strace only continues with a signal if the
-	 * stopping signal is not SIGTRAP.  -brl
-	 */
-	if (current->exit_code) {
-		send_sig(current->exit_code, current, 1);
-		current->exit_code = 0;
-	}
+	ptrace_report_syscall(current_pt_regs());
 }
 
 void send_sigtrap(struct task_struct *tsk, struct pt_regs *regs, int error_code)
