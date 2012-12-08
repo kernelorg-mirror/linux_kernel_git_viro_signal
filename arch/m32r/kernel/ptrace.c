5f7cbbcfbe29f5e75e398ad60795e2408f7e74f7
Al Viro <viro@zeniv.linux.org.uk>
2012-12-07 19:03:28 -0500

    m32r: don't open-code ptrace_report_syscall()
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/m32r/kernel/ptrace.c
+++ arch/m32r/kernel/ptrace.c
@@ -20,7 +20,7 @@
 #include <linux/err.h>
 #include <linux/smp.h>
 #include <linux/errno.h>
-#include <linux/ptrace.h>
+#include <linux/tracehook.h>
 #include <linux/user.h>
 #include <linux/string.h>
 #include <linux/signal.h>
@@ -682,20 +682,5 @@ void do_syscall_trace(void)
 {
 	if (!test_thread_flag(TIF_SYSCALL_TRACE))
 		return;
-	if (!(current->ptrace & PT_PTRACED))
-		return;
-	/* the 0x80 provides a way for the tracing parent to distinguish
-	   between a syscall stop and SIGTRAP delivery */
-	ptrace_notify(SIGTRAP | ((current->ptrace & PT_TRACESYSGOOD)
-				 ? 0x80 : 0));
-
-	/*
-	 * this isn't the same as continuing with a signal, but it will do
-	 * for normal use.  strace only continues with a signal if the
-	 * stopping signal is not SIGTRAP.  -brl
-	 */
-	if (current->exit_code) {
-		send_sig(current->exit_code, current, 1);
-		current->exit_code = 0;
-	}
+	ptrace_report_syscall(current_pt_regs());
 }
